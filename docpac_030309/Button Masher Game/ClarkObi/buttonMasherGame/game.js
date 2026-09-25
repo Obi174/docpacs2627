103495fdd891633119fa577bfb01181dffc016a7
Obi174 <28OClark@live.ytech.edu>
2026-09-24 20:01:11 -0400

    Updating Button masher game

---
+++ docpac_030309/Button Masher Game/ClarkObi/buttonMasherGame/game.js
@@ -0,0 +1,10 @@
+let score = 0
+let time = 20
+let direction = ["up","right","down","left"]
+const scorebox = document.getElementById("scoreBox")
+const timerBox = document.getElementById("timerBox")
+const directionBox = document.getElementById("directionBox")
+const contollerStatus = document.getElementById("contollerStatus")
+    setInterval((time) => {
+        
+    }, interval);
